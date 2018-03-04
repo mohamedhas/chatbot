6c0930d9b4fd42d2a3fa08943ce0c1fcda5d2de7
moh <moh@moh>
2018-03-04 15:31:47 +0100

    first commit

---
+++ src/Replay.hs
@@ -0,0 +1,105 @@
+module Replay (run, ask, io, Replay, Trace, emptyTrace, addAnswer) where
+
+import System.IO.Unsafe
+import Control.Applicative (Applicative(..))
+import Control.Monad       (liftM, ap)
+import Control.Monad.Trans.State.Lazy
+import Control.Monad.Trans.Class
+import Control.Monad.Except
+import Web.Scotty     (ActionM, scotty, get, post, rescue, html, param)
+import Data.Text.Lazy (Text)
+
+{- i ve transformed the trace to a tuple of "(originalTrace, newTrace)" -}
+type Trace r = ([Item r], [Item r])
+
+data Item r = Answer r | Result String
+  deriving (Show, Read)
+
+emptyTrace :: Trace r
+emptyTrace = ([], [])
+
+addAnswer :: Trace r -> r -> Trace r
+addAnswer (trace, output) re = (trace ++ [(Answer re)], output)
+
+type Replay q r  = ReplayT IO q r
+
+type ReplayT m q r  = StateT (Trace r) (ExceptT (q, Trace r) m)
+
+helperFunction :: (Monad m, Show a, Read a) => m a -> ReplayT m q r a
+helperFunction i = do
+  ((rslt, item)) <-  lift (lift (i >>= \x -> return (x, Result $ show x)))
+  trace' <- get
+  case trace' of
+    ([], output)   -> put ([], output ++ [item])
+    (x:xs, output) -> put (xs, output ++ [item])
+  return rslt
+
+liftR :: (Monad m, Show a, Read a) => m a -> ReplayT m q r a
+liftR i = do
+  trace' <- get
+  case trace' of
+    ((Answer aswr):trace, output) -> error $ "undifined"
+    ([], output)                  -> helperFunction i
+    ((Result rslt):trace, output) -> put (trace, output ++ [(Result rslt)]) >>=
+                                      \_ -> return $ read rslt
+
+io :: (Show a, Read a) => IO a -> Replay q r a
+io i = liftR i
+
+ask :: (Monad m) => q -> ReplayT m q r r
+ask qst = do
+  trace' <- get
+  case trace' of
+    ((Answer answr):trace, output) ->
+        put (trace, output ++ [(Answer answr)]) >>= \_ -> return answr
+    ((Result str):trace, output)   -> throwError (qst, (output, []))
+    ([], output)                   -> throwError (qst, (output, []))
+
+
+run :: (Monad m) => ReplayT m q r a -> Trace r -> m (Either (q, Trace r) a)
+run replay trace = runExceptT (evalStateT replay trace)
+
+data HtmlPage = HtmlPage {
+                          head :: String,
+                          body :: [Form]
+                        } deriving (Show, Read)
+data Tag = CForm Form
+    deriving (Show, Read)
+
+data Form = Form [Input]
+    deriving (Show, Read)
+
+data Input = {
+    typeInput :: String,
+    name :: String
+}
+
+getForms :: Text -> [Form]
+
+getInputs :: Form -> [Input]
+
+getAnswers :: ActionM (Trace Answers)
+getAnswers = params >>= fmap Answers >>= return
+
+printTrace :: Trace Answers -> Form
+printTrace trace  = concat $ ( "<form id=\"trace\">" ++ $ fmap
+      (\x -> "<input type=\"hidden\" id=\"" ++ unpack $ fst x ++
+            "\"" "value=\"" ++ unpack $ snd x ++ "\"") fst trace ) ++ "</form>"
+
+getReult :: Form -> Trace Form
+getReult form = filter (~=="<input") $ dropWhile  (~/="<form") rslt
+
+type Web a = Replay Form Answers a
+
+Type Input = Text
+
+type Form = Text
+
+type Answers = (Text, Text)
+
+runWeb :: Web Text -> ActionM ()
+runWeb web = liftIO $ do
+  rslt <-  run web emptyTrace
+  case rslt of
+    (Right page)         -> html (page)
+    (Left (form, trace)) -> case () of
